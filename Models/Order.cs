fc909f6e8d4395dbe069138cdf71fffbaa42e954
Alexander Jacobsen <Alexander_Jacobsen@outlook.com>
2023-02-20 13:48:00 +0100

    Add project files.

---
+++ Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budweg_Sprint1_Test.Models
+
+{
+     public class Order
+        {
+            public int OrderId { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public bool IsCompleted { get; set; }
+
+        }
+}
+
